d8f2ad51fc4dd3524770b2d86557a88c9df8b23e
li409404577 <409404577@qq.com>
2018-11-02 19:19:46 +0800

    liwei

---
+++ build/precache-manifest.3db78177e24a13519cf116513e0d5602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d33cb010ea65640e90d622edb3264968",
+    "url": "/static/media/user-box-bg.d33cb010.png"
+  },
+  {
+    "revision": "bf24a24d1f830f508a2ea9b571df0167",
+    "url": "/static/media/gou.bf24a24d.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10d150feb025d94c4d11",
+    "url": "/static/js/main.10d150fe.chunk.js"
+  },
+  {
+    "revision": "feeb54caee7433e0bffc",
+    "url": "/static/js/1.feeb54ca.chunk.js"
+  },
+  {
+    "revision": "10d150feb025d94c4d11",
+    "url": "/static/css/main.564b9271.chunk.css"
+  },
+  {
+    "revision": "82e628051c210e02c6db91b83b500f37",
+    "url": "/index.html"
+  }
+];
